37c8382703dde3d85d081c444beb2bdfe509079d
Isha Savaliya <155796476+IshaSavaliya25@users.noreply.github.com>
2025-08-28 18:22:52 +0530

    Delete contact_diary_app directory

--- contact_diary_app/android/app/src/main/kotlin/com/example/contact_diary_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.contact_diary_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- contact_diary_app/lib/models/contact.dart
+++
@@ -1,8 +0,0 @@
-class Contact {
-  final String name;
-  final String mobileNumber;
-  final String email;
-
-  Contact(
-      {required this.name, required this.mobileNumber, required this.email});
-}
